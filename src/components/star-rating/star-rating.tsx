0c77cd5aa02506b1beb9072d32f744fa0f0259b4
Jonas Mosmann <jonasmosmann@web.de>
2021-04-25 13:09:11 +0200

    missing stars added

--- src/components/star-rating/star-rating.tsx
+++ src/components/star-rating/star-rating.tsx
@@ -15,6 +15,14 @@ export class StarRating {
           <div class="star-rating">
             <input type="radio" name="rating" id="5-stars" value="5"/>
             <label htmlFor="5-stars"class="star">&#9733;</label>
+            <input type="radio" name="rating" id="4-stars" value="4"/>
+            <label htmlFor="4-stars"class="star">&#9733;</label>
+            <input type="radio" name="rating" id="3-stars" value="3"/>
+            <label htmlFor="3-stars"class="star">&#9733;</label>
+            <input type="radio" name="rating" id="2-stars" value="2"/>
+            <label htmlFor="2-stars"class="star">&#9733;</label>
+            <input type="radio" name="rating" id="1-stars" value="1"/>
+            <label htmlFor="1-stars"class="star">&#9733;</label>
           </div>
         </div>
       </Host>
