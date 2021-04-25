88edfbdaeafcc7a4c3df15a11d612843251068a8
Jonas Mosmann <jonasmosmann@web.de>
2021-04-25 12:35:37 +0200

    star logic image+input test

--- src/components/star-rating/star-rating.tsx
+++ src/components/star-rating/star-rating.tsx
@@ -10,7 +10,13 @@ export class StarRating {
   render() {
     return (
       <Host>
-        <slot></slot>
+        <div id="star-wrapper">
+          <h3 class="please-rate">Bitte bewerten Sie uns!</h3>
+          <div class="star-rating">
+            <input type="radio" name="rating" id="5-stars" value="5"/>
+            <label htmlFor="5-stars"class="star">&#9733;</label>
+          </div>
+        </div>
       </Host>
     );
   }
